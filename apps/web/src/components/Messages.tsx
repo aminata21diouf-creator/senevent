d4a1aba3302847becd35cf3affd1a70de9b91548
Aminata Diouf <aminata21diouf@gmail.com>
2026-06-02 16:42:32 +0000

    annuaire SenEvent en React avec composant EvenementCarte

---
+++ apps/web/src/components/Messages.tsx
@@ -0,0 +1,9 @@
+function Message(){
+    return(
+     <div>
+    <h1 > SenEvent --- Evenements a Dakar </ h1 >
+    <p > Bienvenue dans mon premier composant React . </p >
+</div>
+    );
+}
+export default Message;
